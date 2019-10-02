4c9d6e69adde4eca5b7d574b93ec015fc86dd38a
= <=>
2019-10-02 21:59:50 +0700

    web server commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var userRoute = require('./routes/user.route')
+
+var port = 3000;
+
+var app = express();
+app.set('view engine', 'pug')
+app.set('views', './views')
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.get('/', function(req, res){
+    res.render('index',{
+        name: 'Bakemono'
+    });
+});
+
+app.use('/users', userRoute);
+
+app.listen(port, function(){
+    console.log('Server listening on post ' + port);
+});
